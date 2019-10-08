d878345cd702732f92c0ca307f21d8eba5dd11cc
Oleg Ferenc <olegferenc1@gmail.com>
2019-10-08 19:05:12 +0300

    Array with header items exported to separated file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import headerStyle from './Header.module.css';
 import HeaderItem from "./HeaderItem/HeaderItem";
-
-const categories = [
-    'Новое', 'Лучшее', 'Моя лента', 'Сообщества'
-];
+import { headerItems } from './../../headerItems';
 
 export default class Header extends React.Component {
     render() {
         return (
             <div className={headerStyle.header}>
                 <div className={headerStyle.headerWrapper}>
-                    {categories.map((category, index) => (
+                    {headerItems.map((category, index) => (
                         <HeaderItem key={index} name={category.toUpperCase()}/>
                     ))}
                 </div>
